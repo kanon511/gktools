192116a8608901f8fabaef8d4b8bc404ec9e20c5
Kanon <kanon511@qq.com>
2025-07-22 15:13:26 +0800

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
     {
@@ -14,7 +14,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const Router = createRouter({
-    history: createWebHistory('/gktools/'),
+    history: createWebHashHistory(),
     routes,
 });
 
