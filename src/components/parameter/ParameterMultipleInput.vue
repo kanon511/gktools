395bb19bef2277ff457fefbe9255c556c398c2ac
Kanon <kanon511@qq.com>
2025-07-25 22:13:20 +0800

    fix useGrouping

--- src/components/parameter/ParameterMultipleInput.vue
+++ src/components/parameter/ParameterMultipleInput.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="flex flex-row my-4">
         <FloatLabel variant="on">
-            <InputNumber v-model="parameters['vocal']" :maxFractionDigits="1"
+            <InputNumber v-model="parameters['vocal']" :maxFractionDigits="1" :useGrouping="false"
                 :pt="{ pcInputText: { root: { inputmode: 'decimal' } } }" :invalid="parameters['vocal'] === null"
                 fluid />
             <label for="on_label">Vocal%</label>
         </FloatLabel>
         <FloatLabel class="mx-2" variant="on">
-            <InputNumber v-model="parameters['dance']" :maxFractionDigits="1"
+            <InputNumber v-model="parameters['dance']" :maxFractionDigits="1" :useGrouping="false"
                 :pt="{ pcInputText: { root: { inputmode: 'decimal' } } }" :invalid="parameters['dance'] === null"
                 fluid />
             <label for="on_label">Dance%</label>
         </FloatLabel>
         <FloatLabel variant="on">
-            <InputNumber v-model="parameters['visual']" :maxFractionDigits="1"
+            <InputNumber v-model="parameters['visual']" :maxFractionDigits="1" :useGrouping="false"
                 :pt="{ pcInputText: { root: { inputmode: 'decimal' } } }" :invalid="parameters['visual'] === null"
                 fluid />
             <label for="on_label">Visual%</label>
